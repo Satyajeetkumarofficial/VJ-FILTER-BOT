c3ce7ea29c4d78ac1276fb29e33b189fac41e2e8
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2024-12-28 11:56:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,8 +113,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://mdiskshortner.link/')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '1893a2ce9a3efd947ff2addcc418b705084f35ab')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
